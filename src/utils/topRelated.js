2501aed4f1e4732e9502a175d9516e1a1aff21e7
raquepau <raquepau@gmail.com>
2017-01-21 08:34:29 -0500

    update toprelated with cookie

--- src/utils/topRelated.js
+++ src/utils/topRelated.js
@@ -5,26 +5,31 @@ var createObj = require(__dirname + '/../resources/callbacks.js');
 var checkErrors = require(__dirname + '/../resources/errorHandling.js');
 var parseHtml = require(__dirname + '/../resources/htmlParser.js').parseHtml.bind(null, 'topRelated');
 
-module.exports = function request(keywords, timePeriod, geo, cbFunc){
+module.exports = function request(keywords, timePeriod, geo, cookie, cbFunc) {
 	var obj = createObj(arguments, request);
-	if(!obj.keywords) delete obj.keywords;
+	if (!obj.keywords) delete obj.keywords;
 
 	var error = checkErrors(obj);
-	if(error instanceof Error) return Promise.reject(obj.cbFunc(error));
+	if (error instanceof Error) return Promise.reject(obj.cbFunc(error));
 
-	return Promise.all(promiseArr(obj.keywords || [''], obj.timePeriod, obj.geo))
-	.then(function(results){
+	return Promise.all(promiseArr(obj.keywords || [''], obj.timePeriod, obj.geo, cookie)).then(function (results) {
 		return obj.cbFunc(null, results);
-	})
-	.catch(function(err){
+	}).catch(function (err) {
 		return Promise.reject(obj.cbFunc(err));
 	});
 };
 
-function promiseArr(keywords, timePeriod, country){
-	return keywords.map(function(keyword){
-		return rp(`http://www.google.com/trends/fetchComponent?hl=en-US&q=${keyword}&geo=${country}&cid=TOP_QUERIES_0_0&${timePeriod}`)
-		.then(function(htmlStrings){
+function promiseArr(keywords, timePeriod, country, cookie) {
+	var headers={};
+	if(cookie)
+		headers['Cookie']=cookie;
+	return keywords.map(function (keyword) {
+		//return rp('http://www.google.com/trends/fetchComponent?hl=en-US&q=' + keyword + '&geo=' + country + '&cid=TOP_QUERIES_0_0&' + timePeriod)
+		return rp( {
+	uri: 'http://www.google.com/trends/fetchComponent?hl=en-US&q=' + keyword + '&geo=' + country + '&cid=TOP_QUERIES_0_0&' + timePeriod,
+	headers: headers
+		})
+		.then(function (htmlStrings) {	
 			return parseHtml(htmlStrings);
 		});
 	});
