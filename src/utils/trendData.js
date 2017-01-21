cb2f3a396e84cbf29f5c9fb389ff0f8ea67c8a0c
raquepau <raquepau@gmail.com>
2017-01-21 08:33:16 -0500

    add cookie

--- src/utils/trendData.js
+++ src/utils/trendData.js
@@ -7,13 +7,13 @@ var parseJSON = require(__dirname + '/../resources/htmlParser.js').parseJSON;
 var groupKeywords = require(__dirname + '/../resources/trendDataHelper.js').groupKeywords;
 var reduceArrayDimensions = require(__dirname + '/../resources/trendDataHelper.js').reduceArrayDimensions;
 
-module.exports = function request(keywords, timePeriod, cbFunc){
+module.exports = function request(keywords, timePeriod, cookie, cbFunc){
 	var obj = createObj(arguments, request);
 
 	var error = checkErrors(obj);
 	if(error instanceof Error) return Promise.reject(obj.cbFunc(error));
 
-	return Promise.all(promiseArr(obj.keywords, obj.timePeriod))
+	return Promise.all(promiseArr(obj.keywords, obj.timePeriod, cookie))
 	.then(function(results){
 		return obj.cbFunc(null, reduceArrayDimensions(results));
 	})
@@ -22,9 +22,13 @@ module.exports = function request(keywords, timePeriod, cbFunc){
 	});
 };
 
-function promiseArr(keywords, timePeriod){
+function promiseArr(keywords, timePeriod, cookie){
+	var headers = {};
+	if(cookie)
+		headers['Cookie'] = cookie;
 	return groupKeywords(keywords).map(function(keyword, index, arr){
-		return rp(`http://www.google.com/trends/fetchComponent?q=${keyword}&cid=TIMESERIES_GRAPH_0&export=3&${timePeriod}`)
+		return rp({uri :`http://www.google.com/trends/fetchComponent?q=${keyword}&cid=TIMESERIES_GRAPH_0&export=3&${timePeriod}`,
+			   headers : headers})
 		.then(function(htmlString){
 			return parseJSON(htmlString, arr[index].split(','));
 		});
